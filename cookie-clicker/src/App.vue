075d3900604f74a6107a942de2e2f7e9696e9a9b
Atanas Atanasov <nasko200406@gmail.com>
2022-04-12 22:54:26 +0300

    UPDATE main.js & App.vue
    Fixed names of the components and added some element in App.vue

--- cookie-clicker/src/App.vue
+++ cookie-clicker/src/App.vue
@@ -1,9 +1,46 @@
 <template>
-  
+  <div>
+    <div v-if="loginPressed === false & registrationPressed === false">
+      <button @click="pressLogin">Login</button>
+      <button @click="pressRegistration">Registration</button>
+    </div>
+    
+    <login-component v-if="loginPressed === true"></login-component>
+    
+    <registration-component v-if="registrationPressed === true"></registration-component>
+    
+    <cookie-clicker v-if="cookieClickerOn === true"></cookie-clicker>
+  </div>
 </template>
 
 <script>
+
 export default {
+  data() {
+    return {
+      loginPressed: false,
+      registrationPressed: false,
+      cookieClickerOn: false,
+      userList: [
+        {
+          username: 'John',
+          password: '1234',
+          email: 'john@gmail.com',
+          cookies: 0
+        }
+      ]
+    };
+  },
+
+  methods: {
+    pressLogin() {
+      this.loginPressed = !this.loginPressed;
+    },
+
+    pressRegistration() {
+      this.registrationPressed = !this.registrationPressed;
+    },
+  }
   
 }
 </script>
--- cookie-clicker/src/main.js
+++ cookie-clicker/src/main.js
@@ -1,15 +1,14 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
-import Login from "./components/Login.vue"
-import Registration from "./components/Registration.vue"
+import LoginComponent from "./components/TheLogin.vue"
+import RegistrationComponent from "./components/TheRegistration.vue"
 import CookieClicker from "./components/CookieClicker.vue"
 
 const app = createApp(App);
 
-app.component('login-component', Login);
-app.component('registration-component', Registration);
+app.component('login-component', LoginComponent);
+app.component('registration-component', RegistrationComponent);
 app.component('cookie-clicker', CookieClicker);
 
 app.mount('#app');
-
