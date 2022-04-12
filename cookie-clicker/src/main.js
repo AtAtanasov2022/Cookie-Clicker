ddd4cff7683b67b0e34c467f846b37f822934d64
Atanas Atanasov <nasko200406@gmail.com>
2022-04-12 22:23:40 +0300

    UPDATE main.js
    Connecting the components with the main app

--- cookie-clicker/src/main.js
+++ cookie-clicker/src/main.js
@@ -1,9 +1,15 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
+import Login from "./components/Login.vue"
+import Registration from "./components/Registration.vue"
+import CookieClicker from "./components/CookieClicker.vue"
+
 const app = createApp(App);
 
-
+app.component('login-component', Login);
+app.component('registration-component', Registration);
+app.component('cookie-clicker', CookieClicker);
 
 app.mount('#app');
 
